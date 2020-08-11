d77e437c3115cceada49c3e7fd0aacdf01d1c1e6
Driijko <driijko@gmail.com>
2020-08-11 18:43:05 -0400

    Second pass at palindrome permutations problem, Hash Tables Chapter

--- 2 Hash Tables/hasPalindromePermuation.js
+++ 2 Hash Tables/hasPalindromePermuation.js
@@ -1,42 +1,63 @@
+// function hasPalindromePermutation(theString) {
+
+//   // Edge cases
+//   if (theString.length <= 1) return true;
+
+//   // Iterate through letters, tallying. 
+//   const letters = {};
+//   for (let i = 0 ; i < theString.length; i++) {
+//     const letter = theString[i];
+//     if (letters[letter]) {
+//       letters[letter]++;
+//     }
+//     else {
+//       letters[letter] = 1;
+//     }
+//   }
+
+//   // How many tallies are odd?
+//   let numOfOddTallys = 0;
+//   for (const letter in letters) {
+//     if (letters[letter] % 2 !== 0) {
+//       numOfOddTallys++;
+//       // If there is more than two odd tallies, we know it's not a palindrome. 
+//       if (numOfOddTallys > 2) return false;
+//     }
+//   }
+
+//   // If the string is an even length, there can be zero odd tallys
+//   if (theString.length % 2 === 0) {
+//     if (numOfOddTallys === 0) return true;
+//     else return false;
+//   }
+//   // If the string is an odd length, there can be one odd tally. 
+//   else {
+//     if (numOfOddTallys <= 1) return true;
+//     else return false;
+//   }
+// }
+
 function hasPalindromePermutation(theString) {
 
   // Edge cases
   if (theString.length <= 1) return true;
 
-  // Iterate through letters, tallying. 
-  const letters = {};
+  const oddTallyLetters = new Set();
+
   for (let i = 0 ; i < theString.length; i++) {
     const letter = theString[i];
-    if (letters[letter]) {
-      letters[letter]++;
+    if (oddTallyLetters.has(letter)) {
+      oddTallyLetters.delete(letter);
     }
     else {
-      letters[letter] = 1;
+      oddTallyLetters.add(letter);
     }
   }
 
-  // How many tallies are odd?
-  let numOfOddTallys = 0;
-  for (const letter in letters) {
-    if (letters[letter] % 2 !== 0) {
-      numOfOddTallys++;
-      // If there is more than two odd tallies, we know it's not a palindrome. 
-      if (numOfOddTallys > 2) return false;
-    }
-  }
-
-  // If the string is an even length, there can be zero odd tallys
-  if (theString.length % 2 === 0) {
-    if (numOfOddTallys === 0) return true;
-    else return false;
-  }
-  // If the string is an odd length, there can be one odd tally. 
-  else {
-    if (numOfOddTallys <= 1) return true;
-    else return false;
-  }
+  return oddTallyLetters.size <= 1;
 }
 
-const theString = "ototoi";
+
+const theString = "ototo";
 console.log(hasPalindromePermutation(theString));
 // hasPalindromePermutation(theString)
