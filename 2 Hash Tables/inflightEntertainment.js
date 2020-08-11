c6c01797f9926c01479ee324fe59c3105cad1ef8
Driijko <driijko@gmail.com>
2020-08-11 14:16:10 -0400

    Second pass at inflight entertainment problem, Hash Tables Chapter

--- 2 Hash Tables/inflightEntertainment.js
+++ 2 Hash Tables/inflightEntertainment.js
@@ -4,37 +4,21 @@ function canTwoMoviesFillFlight(movieLengths, flightLength) {
 
   if (movieLengths.length <= 1) return false;
 
-  // Sort movies by length. 
-  const sortedMovieLengths = mergeSort(movieLengths);
+  const movieLengthsSeen = new Set();
 
-  // Loop through all movies
-  for (let i = 0; i < sortedMovieLengths.length; i++) {
-    const firstMovie = sortedMovieLengths[i];
-
-    // If a movie is longer than the flight, we've gone as far as we need to in the sorted array. 
-    if (firstMovie > flightLength) {
-      break;
+  for (let i = 0; i < movieLengths.length; i++) {
+    const firstMovieLength = movieLengths[i];
+    const matchingSecondMovieLength = flightLength - firstMovieLength;
+    if (movieLengthsSeen.has(matchingSecondMovieLength)) {
+      return true;
     }
-    else {
-      for (let j = i + 1; j < sortedMovieLengths.length; j++) {
-        const secondMovie = sortedMovieLengths[j];
-
-        // If a movie is longer than the flight, we've gone as far as we need to in the sorted array. 
-        if (secondMovie > flightLength) {
-          break;
-        }
-        else if (firstMovie + secondMovie === flightLength) {
-          return true;
-        }
-      }
-    }
+    movieLengthsSeen.add(firstMovieLength);
   }
-  
-  
+
   return false;
 }
 
 const movieLengths = [45, 123, 89, 245, 222, 320, 76];
-const flightLength = 134;
+const flightLength = 135;
 
 console.log(canTwoMoviesFillFlight(movieLengths, flightLength));
