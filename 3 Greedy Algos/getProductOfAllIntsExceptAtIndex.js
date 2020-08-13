7d4cde66adb366f3395a3b08e8f1444adf1c0da8
Driijko <driijko@gmail.com>
2020-08-13 16:07:59 -0400

    Third pass at product of all ints except at index problem, Greedy Algorithms Chapter

--- 3 Greedy Algos/getProductOfAllIntsExceptAtIndex.js
+++ 3 Greedy Algos/getProductOfAllIntsExceptAtIndex.js
@@ -1,29 +1,41 @@
 // function getProductsOfAllIntsExceptAtIndex(intArray) {
-
+//   let totalProduct = 1;
 //   const products = [];
-//   for (let i = 0 ; i < intArray.length; i++) {
-//     let product = 1;
-//     for (let j = 0 ; j < intArray.length; j++) {
-//       if (j !== i) {
-//         product *= intArray[j];
-//       }
-//     }
-//     products.push(product);
-//   }  
-
+//   for (let i = 0; i < intArray.length; i++) {
+//     totalProduct *= intArray[i];
+//   }
+//   for (let i = 0; i < intArray.length; i++) {
+//     products[i] = Math.floor(totalProduct / intArray[i]);
+//   }
 //   return products;
 // }
 
 function getProductsOfAllIntsExceptAtIndex(intArray) {
-  let totalProduct = 1;
-  const products = [];
-  for (let i = 0; i < intArray.length; i++) {
-    totalProduct *= intArray[i];
+
+  if (intArray.length < 2) {
+    throw new Error("There must be at least two numbers in array");
   }
-  for (let i = 0; i < intArray.length; i++) {
-    products[i] = Math.floor(totalProduct / intArray[i]);
+
+  const productsOfIntsBeforeIndex = [];
+  let productSoFar = 1;
+  for (let i = 0 ; i < intArray.length; i++) {
+    productsOfIntsBeforeIndex[i] = productSoFar;
+    productSoFar *= intArray[i];
   }
-  return products;
+
+  const productsOfIntsAfterIndex = [];
+  productSoFar = 1;
+  for (let i = intArray.length - 1; i > -1; i--) {
+    productsOfIntsAfterIndex[i] = productSoFar;
+    productSoFar *= intArray[i];
+  }
+
+  for (let i = 0 ; i < intArray.length; i++) {
+    productsOfIntsAfterIndex[i] = productsOfIntsAfterIndex[i] * productsOfIntsBeforeIndex[i];
+  }
+
+  return productsOfIntsAfterIndex;
+
 }
 
 const intArray = [1, 2, 3, 4, 5];
