b48f9952c0ac7ebf7cb15f94af21e36a51b88293
Driijko <driijko@gmail.com>
2020-08-13 13:07:03 -0400

    Second pass apple stock problem, Greedy Algorithms Chapter

--- 3 Greedy Algos/getMaxProfit.js
+++ 3 Greedy Algos/getMaxProfit.js
@@ -1,22 +1,42 @@
-function getMaxProfit(stockPrices) {
-  let lowest = stockPrices[0];
-  let highest = stockPrices[0];
+// function getMaxProfit(stockPrices) {
+//   let lowest = stockPrices[0];
+//   let highest = stockPrices[0];
 
-  for (let i = 1 ; i < stockPrices.length; i++) {
-    const stockPrice = stockPrices[i];
-    if (stockPrice < lowest) {
-      lowest = stockPrice;
-      highest = stockPrice;
-    }
-    else if (stockPrice > highest) {
-      highest = stockPrice;
+//   for (let i = 1 ; i < stockPrices.length; i++) {
+//     const stockPrice = stockPrices[i];
+//     if (stockPrice < lowest) {
+//       lowest = stockPrice;
+//       highest = stockPrice;
+//     }
+//     else if (stockPrice > highest) {
+//       highest = stockPrice;
+//     }
+//   }
+
+//   return highest - lowest;
+// }
+
+function getMaxProfit(stockPrices) {
+
+  if (stockPrices.length <= 1) return new Error();
+
+  let maxProfit = stockPrices[1] - stockPrices[0];
+
+  for (let earlierTime = 0; earlierTime < stockPrices.length; earlierTime++) {
+    const buyPrice = stockPrices[earlierTime];
+
+    for (let laterTime = earlierTime + 1; laterTime < stockPrices.length; laterTime++) {
+      const sellPrice = stockPrices[laterTime];
+      if (sellPrice > buyPrice) {
+        const potentialProfit = sellPrice - buyPrice;
+        maxProfit = Math.max(maxProfit, potentialProfit);
+      }
     }
   }
-
-  return highest - lowest;
+  return maxProfit;
 }
 
-const stockPrices = [10, 7, 5, 8, 11, 9];
+const stockPrices = [10, 7, 5, 8, 11, 9, 15];
 console.log(getMaxProfit(stockPrices));
 
 function translateIndexToTime(index) {
