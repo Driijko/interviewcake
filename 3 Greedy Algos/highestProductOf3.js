1694b78b6b22b63284876f60fc069f188a55f89c
Driijko <driijko@gmail.com>
2020-08-13 15:20:42 -0400

    Second pass highest product of 3 problem, Greedy Algorithms Chapter

--- 3 Greedy Algos/highestProductOf3.js
+++ 3 Greedy Algos/highestProductOf3.js
@@ -4,25 +4,44 @@ function highestProductOf3(arrayOfInts) {
     throw new Error("Array must have at least three integers");
   }
 
-  const highestInts = [arrayOfInts[0], arrayOfInts[1], arrayOfInts[2]];
-  for (let i = 3; i < arrayOfInts.length; i++) {
+  let highestProductOf3 = arrayOfInts[0] * arrayOfInts[1] * arrayOfInts[2];
+  let highestProductOf2 = arrayOfInts[0] * arrayOfInts[1];
+  let highest = Math.max(arrayOfInts[0], arrayOfInts[1]);
+  let lowestProductOf2 = arrayOfInts[0] * arrayOfInts[1];
+  let lowest = Math.min(arrayOfInts[0], arrayOfInts[1]);;
+
+  for (let i = 2 ; i < arrayOfInts.length; i++) {
     const int = arrayOfInts[i];
-    let biggestDifference = 0;
-    let bestPick = null;
-    for (let j = 0; j < highestInts.length; j++) {
-      const highInt = highestInts[j];
-      if (int > highInt && int - highInt > biggestDifference) {
-        biggestDifference = int - highInt;
-        bestPick = j;
-      }
-    }
-    if (bestPick !== null) {
-      highestInts[bestPick] = int;
-    }
+
+    highestProductOf3 = (
+      Math.max(
+        highestProductOf2 * int,
+        lowestProductOf2 * int,
+        highestProductOf3
+      )
+    );
+    
+    highestProductOf2 = (
+      Math.max(
+        highest * int,
+        lowest * int,
+        highestProductOf2
+      )
+    );
+
+    lowestProductOf2 = (
+      Math.min(
+        lowest * int,
+        highest * int,
+        lowestProductOf2
+      )
+    );
+
+    highest = Math.max(int, highest);
+    lowest = Math.min(int, lowest);
   }
-  return highestInts.reduce((accumulator, currentValue) => accumulator * currentValue);
+  return highestProductOf3;
 }
 
-// const arrayOfInts = [13, 22, 54, 2, 58, 91, 0, 800];
-const arrayOfInts = [1, 2, 3, 4];
+const arrayOfInts = [10, 20, 1, 30, -50, -50];
 console.log(highestProductOf3(arrayOfInts));
