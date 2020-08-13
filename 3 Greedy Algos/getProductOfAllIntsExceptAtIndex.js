f38a3e670c17c40afb6c7b612978f19985fe117a
Driijko <driijko@gmail.com>
2020-08-13 15:33:47 -0400

    Second pass at product of all ints except at index problem, Greedy Algorithms Chapter

--- 3 Greedy Algos/getProductOfAllIntsExceptAtIndex.js
+++ 3 Greedy Algos/getProductOfAllIntsExceptAtIndex.js
@@ -1,16 +1,28 @@
+// function getProductsOfAllIntsExceptAtIndex(intArray) {
+
+//   const products = [];
+//   for (let i = 0 ; i < intArray.length; i++) {
+//     let product = 1;
+//     for (let j = 0 ; j < intArray.length; j++) {
+//       if (j !== i) {
+//         product *= intArray[j];
+//       }
+//     }
+//     products.push(product);
+//   }  
+
+//   return products;
+// }
+
 function getProductsOfAllIntsExceptAtIndex(intArray) {
-
+  let totalProduct = 1;
   const products = [];
-  for (let i = 0 ; i < intArray.length; i++) {
-    let product = 1;
-    for (let j = 0 ; j < intArray.length; j++) {
-      if (j !== i) {
-        product *= intArray[j];
-      }
-    }
-    products.push(product);
-  }  
-
+  for (let i = 0; i < intArray.length; i++) {
+    totalProduct *= intArray[i];
+  }
+  for (let i = 0; i < intArray.length; i++) {
+    products[i] = Math.floor(totalProduct / intArray[i]);
+  }
   return products;
 }
 
