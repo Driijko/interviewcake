738628f0646e7b5581f123eb192a7e1229a2960e
Driijko <driijko@gmail.com>
2020-08-14 13:27:38 -0400

    Second pass at shuffle in place problem, Greedy Algorithms Chapter

--- 3 Greedy Algos/getRandom.js
+++ 3 Greedy Algos/getRandom.js
@@ -2,16 +2,26 @@ function getRandom(floor, ceiling) {
   return Math.floor(Math.random() * (ceiling - floor + 1)) + floor;
 }
 
+// function shuffle(array) {
+//   for (let i = 0 ; i < array.length; i++) {
+//     const newIndex = getRandom(0, array.length - 1);
+//     const temp = array[newIndex];
+//     array[newIndex] = array[i];
+//     array[i] = temp;
+//   }
+//   return array;
+// }
+
 function shuffle(array) {
-  for (let i = 0 ; i < array.length; i++) {
-    const newIndex = getRandom(0, array.length - 1);
-    const temp = array[newIndex];
-    array[newIndex] = array[i];
-    array[i] = temp;
+  const iterations = array.length
+  const newOrder = [];
+  for(let i = 0; i < iterations; i++) {
+    newOrder[i] = array.splice(getRandom(0, array.length - 1), 1)[0];
+    console.log(array);
   }
-  return array;
+  return newOrder;
 }
 
-const sample = [1, 2, 3, 4, 5];
+const sample = [1, 12, 3, 4, 5];
 
 console.log(shuffle(sample));
