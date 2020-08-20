2df9914a8cb5f7bc71c9d8aa5f96dde3d22617ff
Driijko <driijko@gmail.com>
2020-08-20 13:11:58 -0400

    Second pass at is tree binary search tree problem, Trees and Graphs Chapter

--- 5 Trees & Graphs/isBinarySearchTree.js
+++ 5 Trees & Graphs/isBinarySearchTree.js
@@ -16,32 +16,38 @@ class BinaryTreeNode {
   }
 }
 
-function isBinarySearchTree(node) {
-  if (node.left) {
-    if (node.left.value >= node.value) {
-      return false;
+function isBinarySearchTree(treeRoot) {
+
+  if (!(treeRoot)) return false;
+
+  function recursiveCall(node, lowerBound=-Infinity, upperBound=Infinity) {
+    if (node.left) {
+      if (node.left.value >= node.value || node.left.value <= lowerBound) {
+        return false;
+      }
+      else if (recursiveCall(node.left, lowerBound, node.value) === false) {
+        return false;
+      }
     }
-    else if (isBinarySearchTree(node.left) === false) {
-      return false;
+    if (node.right) {
+      if (node.right.value <= node.value || node.right.value >= upperBound) {
+        return false;
+      }
+      else if (recursiveCall(node.right, node.value, upperBound) === false) {
+        return false;
+      }
     }
+    return true;
   }
-  if (node.right) {
-    if (node.right.value <= node.value) {
-      return false;
-    }
-    else if (isBinarySearchTree(node.right) === false) {
-      return false;
-    }
-  }
-  return true;
+  return recursiveCall(treeRoot);
 }
 
-let treeRoot = new BinaryTreeNode(50);
-let leftNode = treeRoot.insertLeft(30);
-leftNode.insertLeft(10);
+const treeRoot = new BinaryTreeNode(50);
+const leftNode = treeRoot.insertLeft(30);
+leftNode.insertLeft(20);
 leftNode.insertRight(40);
-let rightNode = treeRoot.insertRight(70);
-rightNode.insertLeft(60);
-rightNode.insertRight(80);
+const rightNode = treeRoot.insertRight(80);
+rightNode.insertLeft(70);
+rightNode.insertRight(90);
 
 console.log(isBinarySearchTree(treeRoot));
