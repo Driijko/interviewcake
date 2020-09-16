6c689a51f9b2933ac00c7c863afce35edf976f7c
Driijko <driijko@gmail.com>
2020-09-16 16:48:15 -0400

    Second pass at mesh message problem, Graphs and Trees Chapter

---
+++ 5 Trees & Graphs/meshMessage.js
@@ -0,0 +1,65 @@
+class Queue {
+  constructor() {
+    this.queue = [];
+    this.size = 0;
+  }
+
+  enqueue(item) {
+    this.queue.unshift(item);
+    this.size += 1;
+  }
+
+  dequeue() {
+    this.size -= 1;
+    return this.queue.pop();
+  }
+}
+
+const network = {
+  'Min'     : ['Omar', 'Jayden', 'William'],
+  'William' : ['Min', 'Noam'],
+  'Jayden'  : ['Min', 'Ren', 'Noam'],
+  'Ren'     : ['Jayden', 'Omar'],
+  'Amelia'  : ['Jayden', 'Adam', 'Miguel'],
+  'Adam'    : ['Amelia', 'Miguel', 'Sofia', 'Lucas'],
+  'Miguel'  : ['Amelia', 'Adam', 'Liam', 'Nathan'],
+  'Noam'    : ['Nathan', 'Jayden', 'William'],
+  'Omar'    : ['Ren', 'Min', 'Scott']
+};
+
+function getPath(graph, startNode, endNode) {
+  
+  if (startNode === endNode) {
+    return [startNode];
+  }
+  
+  const toCheck = new Queue();
+  toCheck.enqueue([startNode]);
+  const alreadyChecked = new Set();
+
+  while (toCheck.size > 0) {
+    const path = toCheck.queue[toCheck.size - 1];
+    const currentNode = path[path.length - 1];
+    for (const person of graph[currentNode]) {
+      if (person === endNode) {
+        path.push(person);
+        return path;
+      }
+      else if (alreadyChecked.has(person) === false && graph[person]) {
+        const newPath = [...path];
+        newPath.push(person);
+        toCheck.enqueue(newPath);
+      }
+    }
+    alreadyChecked.add(currentNode);
+    toCheck.dequeue();
+  }
+  if (graph[endNode]) {
+    return null;
+  }
+  else {
+    throw new Error(`Unable to find a path from ${startNode} to ${endNode}`)
+  }
+}
+
+console.log(getPath(network, 'Min', 'Noam'));
