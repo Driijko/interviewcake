e216b25e2acd0ffd766dc28f186f01d5dbcc1ac0
Driijko <driijko@gmail.com>
2020-09-21 19:33:16 -0400

    Second pass at Max Stack problem, Queues and Stacks Chapter

--- 7 Queues and Stacks/MaxStack.js
+++ 7 Queues and Stacks/MaxStack.js
@@ -30,23 +30,69 @@ class Stack {
   }
 }
 
-class MaxStack extends Stack{
+class MaxStack {
   constructor() {
-    super();
+
+    // Initialize an empty stack
+    this.items = [];
+    this.max = -Infinity;
+  }
+
+  // Push a new item onto the stack
+  push(item) {
+    this.items.push(item);
+    this.max = Math.max(item, this.max);
+  }
+
+  // Remove and return the last item
+  pop() {
+
+    // If the stack is empty, return null
+    // (It would also be reasonable to throw an exception)
+    if (!this.items.length) {
+      return null;
+    }
+    const poppedItem = this.items.pop();
+    if (this.max === poppedItem) {
+      this.max = -Infinity;
+      for (const item of this.items) {
+        this.max = Math.max(item, this.max);
+      }
+    }
+    return poppedItem;
+  }
+
+  // Returns the last item without removing it
+  peek() {
+    if (!this.items.length) {
+      return null;
+    }
+    return this.items[this.items.length - 1];
   }
 
   getMax() {
-    let max = -Infinity;
-    for (const item of this.items) {
-      max = Math.max(item, max);
-    }
-    return max;
+    return this.max;
   }
 }
+
+// class MaxStack extends Stack{
+//   constructor() {
+//     super();
+//   }
+
+//   getMax() {
+//     let max = -Infinity;
+//     for (const item of this.items) {
+//       max = Math.max(item, max);
+//     }
+//     return max;
+//   }
+// }
 
 const maxStack = new MaxStack();
 maxStack.push(5);
 maxStack.push(7);
 maxStack.push(145);
 maxStack.push(5656);
+maxStack.pop();
 console.log(maxStack.getMax());
