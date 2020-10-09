eae17c8b7da36964393ca328c2075937a837d888
Driijko <driijko@gmail.com>
2020-10-09 17:33:59 -0400

    Third pass at compress url list, System Design Chapter

--- 9 System Design/compressUrlList.js
+++ 9 System Design/compressUrlList.js
@@ -3,13 +3,14 @@ const visited = {};
 function addUrl(url) {
   visited[url[0]] = {};
   let nestedPosition = visited[url[0]];
-  for (let i = 1 ; i < url.length ; i++) {
+  for (let i = 1 ; i < url.length; i++) {
     const currentChar = url[i];
     if (!(nestedPosition[currentChar])) {
       nestedPosition[currentChar] = {};
     }
     nestedPosition = nestedPosition[currentChar];
   }
+  nestedPosition["*"] = true;
 }
 
 addUrl("www.website");
